0ab04fcea542bf13f63975fd866661fc9f0008ec
1000copy <1000copy@gmail.com>
2018-08-22 09:57:37 +0800

    I love arrow function when I find THIS in async function in class is not my expect ,but arrow function do it right !

--- backserver.js
+++ backserver.js
@@ -22,90 +22,140 @@ const defaultTodo = [
   	return -1
   }
   var mongo = require('mongodb')
-  function insertDoc(subject,callback){
-    const MongoClient = mongo.MongoClient;
-    const connectionString = 'mongodb://localhost:27017';
-    (async () => {
-        const client = await MongoClient.connect(connectionString,
+  class M {
+    constructor(){
+      this.getCol = ()=>{
+        return this.dbo.collection("todo")
+      }
+      this.m_open = async ()=>{
+        const connectionString = 'mongodb://localhost:27017';
+        this.client = await mongo.MongoClient.connect(connectionString,
             { useNewUrlParser: true });
-        const dbo = client.db('todos');
-        try {
-           var res = await dbo.collection('todo').insertOne(
-            {subject:subject})
-           callback(undefined,res.insertedId)
-        }
-        catch(err){
-          callback(err)
+        this.dbo = this.client.db('todos');
+      }
+      this.insertDoc = async (subject)=>{
+        return  this.template(async ()=>{
+           var res = await this.getCol().insertOne({subject:subject})
+           return res.insertedId
+        })
+        // await this.m_open()
+        // try {
+        //    var res = await this.getCol().insertOne({subject:subject})
+        //    return res.insertedId
+        // }
+        // finally {
+        //   this.client.close();
+        // }
+      }
+      this.deleteDoc = async (_id)=>{
+        return  this.template(async ()=>{
+          await this.getCol().deleteMany({_id:new mongo.ObjectID(_id)})
+        })
+          // await this.m_open()
+          // try{
+          //   await this.getCol().deleteMany({_id:new mongo.ObjectID(_id)})
+          // }
+          // finally {
+          //     this.client.close();
+          // }
+      }
+      this.template = async (cb)=>{
+         try {
+           await this.m_open()
+           // return cb.apply(this)
+           return cb()
         }
         finally {
-            client.close();
+            this.client.close();
         }
-    })().catch(err => console.error(err));
+      }
+      this.allDoc = async ()=>{
+        return this.template(async ()=>{
+           var r = await this.getCol().find().toArray()
+           var ts = []
+           for (var i = 0; i < r.length; i++) {
+             ts.push({id:r[i]._id,subject:r[i].subject})
+           }
+           return ts
+        })
+        // try {
+        //    await this.m_open()
+        //    var r = await this.getCol().find().toArray()
+        //    var ts = []
+        //    for (var i = 0; i < r.length; i++) {
+        //      ts.push({id:r[i]._id,subject:r[i].subject})
+        //    }
+        //    return ts
+        // }
+        // finally {
+        //     this.client.close();
+        // }
+      }
+    }
   }
-  function deleteDoc(_id,callback){
-  	const MongoClient = mongo.MongoClient;
-  	const connectionString = 'mongodb://localhost:27017';
-  	(async () => {
-  	    const client = await MongoClient.connect(connectionString,
-  	        { useNewUrlParser: true });
-  	    const dbo = client.db('todos');
-  	    try {
-                var myquery = {_id:new mongo.ObjectID(_id)}
-                var r = await dbo.collection("todo").deleteMany(myquery)
-          }
-          catch(err){
-  	    	callback(err)
-  	    }
-  	    finally {
-              client.close();
-              callback()
-          }
-  	    
-  	})().catch(err => console.error(err));
+  
+  class M2 {
+    getCol(_this){
+      return _this.dbo.collection("todo")
+    }
+    async  insertDoc(_this,subject){
+      await _this.m_open(_this)
+      try {
+         var res = await _this.getCol(_this).insertOne({subject:subject})
+         return res.insertedId
+      }
+      finally {
+        _this.client.close();
+      }
+    }
+    async  deleteDoc(_this,_id){
+      await _this.m_open(_this)
+      try{
+        await _this.getCol(_this).deleteMany({_id:new mongo.ObjectID(_id)})
+      }
+      finally {
+          _this.client.close();
+      }
+    }
+    async  m_open(_this){
+      const MongoClient = mongo.MongoClient;
+      const connectionString = 'mongodb://localhost:27017';
+      _this.client = await MongoClient.connect(connectionString,
+          { useNewUrlParser: true });
+      _this.dbo = _this.client.db('todos');
+    }
+    async  allDoc(_this){
+      try {
+          await _this.m_open(_this)
+           var r = await _this.getCol(_this).find().toArray()
+           var ts = []
+           for (var i = 0; i < r.length; i++) {
+             ts.push({id:r[i]._id,subject:r[i].subject})
+           }
+           return ts
+      }
+      finally {
+          _this.client.close();
+      }
+    }
   }
-  function allDoc(callback){
-  	const MongoClient = mongo.MongoClient;
-  	const connectionString = 'mongodb://localhost:27017';
-  	(async () => {
-  	    const client = await MongoClient.connect(connectionString,
-  	        { useNewUrlParser: true });
-  	    const dbo = client.db('todos');
-  	    try {
-               var r = await dbo.collection("todo").find().toArray()
-               var ts = []
-               for (var i = 0; i < r.length; i++) {
-                 ts.push({id:r[i]._id,subject:r[i].subject})
-               }
-               callback(undefined,ts)
-          }
-          catch(err){
-  	    	callback(err)
-  	    }
-  	    finally {
-              client.close();
-          }
-  	    
-  	})().catch(err => console.error(err));
-  }
-  app.delete('/api/todo/:id', function (req, res) {
+  var m = new M()
+  app.delete('/api/todo/:id', async function (req, res) {
   	var userkey = req.params.id
-  	deleteDoc(userkey,function(){
-  		todos.splice(indexById(userkey),1)
-    	res.end( JSON.stringify(todos));
-  	})
+    await m.deleteDoc(userkey)
+    todos.splice(indexById(userkey),1)
+    res.end( JSON.stringify(todos));
   })
-  app.get('/api/todos', function (req, res) {
-  	allDoc(function(err,todos){
-  		res.end( JSON.stringify(todos));	
-  	})
+  app.get('/api/todos', async function (req, res) {
+  	var todos = await m.allDoc()
+    res.end( JSON.stringify(todos));  
   })
-  app.post('/api/todo', function (req, res) {
-    insertDoc(req.body.subject,function(err,_id){
-    	var obj ={id:_id,subject:req.body.subject}
-		todos.push(obj)
-		res.end(JSON.stringify(obj))
-	    rs()
-  	})
+  app.post('/api/todo', async function (req, res) {
+    var _id = await m.insertDoc(req.body.subject)
+    var obj ={id:_id,subject:req.body.subject}
+    todos.push(obj)
+    res.end(JSON.stringify(obj))
+    rs()
   })
   var server = app.listen(8181, function () {
     var host = server.address().address
